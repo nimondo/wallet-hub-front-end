aec07dbd3d84f400ad5a73a0fa41e224cae41a70
Znimed <znimed@gmail.com>
2020-02-19 17:03:52 +0000

    Add auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wallet-hub';
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyAoDQnyhe53BK2A_x1ZNmMts80qDH9A3GI",
+      authDomain: "bookcatalog-1473e.firebaseapp.com",
+      databaseURL: "https://bookcatalog-1473e.firebaseio.com",
+      projectId: "bookcatalog-1473e",
+      storageBucket: "",
+      messagingSenderId: "596588004159",
+      appId: "1:596588004159:web:4cab6e426b225772"
+    };
+    firebase.initializeApp(config);
+  }
 }
